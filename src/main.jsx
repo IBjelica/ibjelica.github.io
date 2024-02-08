96aed4ae3caac205e24afb41fd91b9d2448fa578
Igor Bjelica <igorjbjelica@gmail.com>
2024-02-08 23:15:40 +0100

    router debugging

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ntsh">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
