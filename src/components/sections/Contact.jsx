3ed16a4f6d32eedceb8a3a6406943e01bdcb1e15
Igor Bjelica <igorjbjelica@gmail.com>
2024-03-20 11:08:21 +0100

    Socials one line

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -7,21 +7,19 @@ const Contact = (props) => {
 
   return (
     <div className="contact-section">
-      <div className="content">
-        <h1 className="title">
-            we <Word text="EMBRACE" /> the <Word text="ART" /> of the <Word text="UNSEEN" />
-        </h1>
-        <p>
-            We believe that the true magic" of web design" and development" lies beneath the surface. While others may only
-            see the final results, we take pride in the dedication", the intricate process", and the creative gymnastics
-            that unfold within our team.
-        </p>
-        <p>
-            Our approach revolves around a commitment to understanding and constructing value" by immersing ourselves in
-            our customers&#39; unique" needs. For us, design" transcends mere aesthetics; it&#39;s a profound connection
-            rooted in empathy", compassion", and an authentic concern for the individual we work with.
-        </p>
-      </div>
+      <h1 className="title">
+        we <Word text="EMBRACE" /> the <Word text="ART" /> of the <Word text="UNSEEN" />
+      </h1>
+      <p>
+        We believe that the true magic" of web design" and development" lies beneath the surface. While others may only
+        see the final results, we take pride in the dedication", the intricate process", and the creative gymnastics
+        that unfold within our team.
+      </p>
+      <p>
+        Our approach revolves around a commitment to understanding and constructing value" by immersing ourselves in
+        our customers&#39; unique" needs. For us, design" transcends mere aesthetics; it&#39;s a profound connection
+        rooted in empathy", compassion", and an authentic concern for the individual we work with.
+      </p>
     </div>
   );
 }
